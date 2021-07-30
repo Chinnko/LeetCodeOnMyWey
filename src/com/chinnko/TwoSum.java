793bc9a4e0dea4bfb65189f681933c5e398f0fe6
Chinnko <chinn96@163.com>
2021-07-30 09:20:11 +0800

    [add]Two Sum   Add Two Numbers

---
+++ src/com/chinnko/TwoSum.java
@@ -0,0 +1,28 @@
+package com.chinnko;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] ints = twoSum(new int[]{2, 7, 11, 15}, 9);
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] outPuts = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int end = target - nums[i];
+            for (int i1 = i + 1; i1 < nums.length; i1++) {
+                if (nums[i1] == end) {
+                    outPuts[0] = i;
+                    outPuts[1] = i1;
+                    return outPuts;
+
+                }
+            }
+        }
+        return outPuts;
+
+    }
+}
