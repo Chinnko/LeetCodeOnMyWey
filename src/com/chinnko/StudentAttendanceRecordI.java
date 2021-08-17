5dd79d016a4dd5c482d30572c539d5ea562e81e7
chenhaonan@win-stock.com.cn <Chen7777>
2021-08-17 17:21:34 +0800

    add

---
+++ src/com/chinnko/StudentAttendanceRecordI.java
@@ -0,0 +1,31 @@
+import javax.print.DocFlavor;
+
+public class StudentAttendanceRecordI {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean checkRecord(String s) {
+        int l = 0, a = 0;
+        for (char c : s.toCharArray()) {
+            if (c == 'A') {
+                if (a == 1) {
+                    return false;
+                }
+                a = a + 1;
+            }
+            if (c == 'L') {
+                if (l == 2) {
+                    return false;
+                }
+                l = l + 1;
+                continue;
+            }
+            if (l != 0) {
+                l = 0;
+            }
+        }
+        return true;
+    }
+
+}
